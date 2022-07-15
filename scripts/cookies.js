6a3618e66747348f739cc4d37a53eae715365f74
Nabil Adem <nabiladam7@gmail.com>
2022-07-15 14:15:38 -0400

    display cookies in output

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,19 +1,26 @@
-var exp = document.getElementById("export");
-
-if (exp) {
-    exp.addEventListener("click", exportCookies);
-}
-
-var imp = document.getElementById("import");
-
-if (imp) {
-    imp.addEventListener("click", importCookies);
-}
-
-function exportCookies() {
-    alert(document.cookie.length);
-}
-
-function importCookies() {
-    alert(document.cookie.length);
-}
+document.getElementById('id_button_export').onclick = () => {
+    chrome.cookies.getAll(getDetails('getAll'), ((cookies) => {
+      let text ='Number of cookies=' + cookies.length + '\n';
+      for (let cookie of cookies) {
+        text += 'domain=' + cookie.domain + ',path=' + cookie.path + ',name=' + cookie.name + ',value=' + cookie.value + '\n';
+      }
+      document.getElementById('id_text').value = text;
+    }));
+  }
+  
+  function getDetails(kind){
+    let domain = document.getElementById('id_domain').value;
+    let details = {};
+  
+    switch(kind){
+    case 'getAll':
+      if (domain != ''){
+        details['domain'] = domain;
+      }
+      if (name != ''){
+        details['name'] = name;
+      }
+      break;
+    }
+    return details;
+  }
